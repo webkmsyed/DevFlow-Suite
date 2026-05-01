0f6f23526ac008886987fb822dfc07ccbc671b85
Khishamuddin <kmsyed2003@gmail.com>
2026-05-01 10:28:18 +0530

    feat(general-modular): link all folder and task operations for general workspace

--- features/subTabTasks/general/generalTaskIndex.js
+++ features/subTabTasks/general/generalTaskIndex.js
@@ -2,11 +2,16 @@
 const { registerGeneralTaskTag } = require('./generalTaskTag');
 const { registerGeneralTaskDelete } = require('./generalTaskDelete');
 const { registerGeneralTaskCopy } = require('./generalTaskCopy');
+const { registerGeneralTaskPriority } = require('./generalTaskPriority');
+const { registerGeneralTaskAddTo } = require('./generalTaskAddTo');
 
 function registerGeneralTaskOps(context, todoProvider) {
+    // 🔥 Registering all task-level menu logic
     registerGeneralTaskTag(context, todoProvider);
     registerGeneralTaskDelete(context, todoProvider);
     registerGeneralTaskCopy(context, todoProvider);
+    registerGeneralTaskPriority(context, todoProvider);
+    registerGeneralTaskAddTo(context, todoProvider);
 }
 
 module.exports = { registerGeneralTaskOps };
--- features/subTabs/general/generalTabIndex.js
+++ features/subTabs/general/generalTabIndex.js
@@ -2,11 +2,14 @@
 const { registerGeneralTabPriority } = require('./generalTabPriority');
 const { registerGeneralTabExport } = require('./generalTabExport');
 const { registerGeneralTabSorting } = require('./generalTabSorting');
+const { registerGeneralTabDelete } = require('./generalTabDelete');
 
-function registerGeneralTabOps(context, todoProvider) {
+function registerGeneralTabOps(context, todoProvider, scanWorkspace) {
+    // 🔥 Registering all folder-level buttons logic
     registerGeneralTabPriority(context, todoProvider);
     registerGeneralTabExport(context, todoProvider);
     registerGeneralTabSorting(context, todoProvider);
+    registerGeneralTabDelete(context, todoProvider, scanWorkspace);
 }
 
 module.exports = { registerGeneralTabOps };
